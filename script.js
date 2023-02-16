d18949a73b1981b55cb2218ce64aac76bbce9455
JaypeeLan <jplaniran01@gmail.com>
2023-02-16 02:21:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// prompt user to select media stream, pass to video elem and play
+const selectMediaStream = async () => {
+  try {
+    // request the user to select window, tab,screen
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+
+    //    set the html video source attribute to the user video
+    videoElement.srcObject = mediaStream;
+
+    // when the data is loaded, play the video
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    alert("Error: ", error);
+  }
+};
+
+button.addEventListener("click", async () => {
+  button.disabled = true;
+  // --- start PiP
+  await videoElement.requestPictureInPicture();
+
+  button.disabled = false;
+});
+
+selectMediaStream();
